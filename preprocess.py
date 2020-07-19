decc6086ef73aaf949de7574fbc332d37f8facf4
Harsh Panwar <harshpanwar9524@gmail.com>
2020-07-20 00:56:58 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -4,23 +4,20 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-
+import pandas as pd
 # Set parameters here 
 INPUT_SIZE = (224, 224)
-mapping = {'normal': 0, 'pneumonia': 1, 'COVID-19': 2}
+mapping = {'covid': 0, 'non-covid': 1}
 
 # train/valid/test .txt files
-train_filepath = 'train_split_v3.txt'
-valid_filepath = 'valid_split_v3.txt'
-test_filepath = 'test_split_v3.txt'
+train_filepath = '/kaggle/working/covid_ct_dataset/train/train.csv'
+valid_filepath = '/kaggle/working/covid_ct_dataset/valid/valid.csv'
+test_filepath = '/kaggle/working/covid_ct_dataset/test/test.csv'
 
 # load in the train and test files
-file = open(train_filepath, 'r') 
-trainfiles = file.readlines()
-file = open(valid_filepath, 'r')
-validfiles = file.readlines() 
-file = open(test_filepath, 'r')
-testfiles = file.readlines()
+trainfiles = pd.read_csv(train_filepath,header=None)
+validfiles = pd.read_csv(valid_filepath,header=None)
+testfiles = pd.read_csv(test_filepath,header=None)
 
 print('Total samples for train: ', len(trainfiles))
 print('Total samples for valid: ', len(validfiles))
@@ -41,35 +38,35 @@ y_test = []
 
 # Create ./data/test - ./data/train - ./data/valid directories yourself
 for i in range(len(testfiles)):
-    test_i = testfiles[i].split()
-    imgpath = test_i[1]
-    img = cv2.imread(os.path.join(r'./data/test', imgpath))
+    # test_i = testfiles[i]
+    imgpath = testfiles[1][i]
+    img = cv2.imread(os.path.join(r'/kaggle/working/covid_ct_dataset/test', imgpath))
     img = cv2.resize(img, INPUT_SIZE) # resize
     img = img.astype('float32') / 255.0
     x_test.append(img)
-    y_test.append(mapping[test_i[2]])
+    y_test.append(mapping[testfiles[2][i]])
 
 print('Shape of test images: ', x_test[0].shape)
 
 for i in range(len(validfiles)):
-    valid_i = validfiles[i].split()
-    imgpath = valid_i[1]
-    img = cv2.imread(os.path.join(r'./data/valid', imgpath))
+    # valid_i = validfiles[i].split()
+    imgpath = validfiles[1][i]
+    img = cv2.imread(os.path.join(r'/kaggle/working/covid_ct_dataset/valid', imgpath))
     img = cv2.resize(img, INPUT_SIZE) # resize
     img = img.astype('float32') / 255.0
     x_valid.append(img)
-    y_valid.append(mapping[valid_i[2]])
+    y_valid.append(mapping[validfiles[2][i]])
 
 print('Shape of valid images: ', x_valid[0].shape)
 
 for i in range(len(trainfiles)):
-    train_i = trainfiles[i].split()
-    imgpath = train_i[1]
-    img = cv2.imread(os.path.join(r'./data/train', imgpath))
+    # train_i = trainfiles[i].split()
+    imgpath = trainfiles[1][i]
+    img = cv2.imread(os.path.join(r'/kaggle/working/covid_ct_dataset/train', imgpath))
     img = cv2.resize(img, INPUT_SIZE) # resize
     img = img.astype('float32') / 255.0
     x_train.append(img)
-    y_train.append(mapping[train_i[2]])
+    y_train.append(mapping[trainfiles[2][i]])
 
 print('Shape of train images: ', x_train[0].shape)
 
